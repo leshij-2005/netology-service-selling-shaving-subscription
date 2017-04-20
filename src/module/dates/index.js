7d40202a86472b933eb5e4bb8a0cda7a13bc116e
Alexey Ershov <alershov3@gmail.com>
2017-04-20 12:40:59 +0500

    add min dates for datepickers;

--- src/module/dates/index.js
+++ src/module/dates/index.js
@@ -2,12 +2,19 @@ saleApp
   .component('dates', {
     templateUrl: 'src/module/dates/template/index.html',
     controller: function($rootScope, $scope, customer, CUSTOMER_EVENTS) {
+      $scope.today = new Date();
+      $scope.minSecondDate = new Date();
+
       $scope.customer = customer;
 
       $scope.select = function(date, index = 0) {
         customer.selectDate(date, index);
 
         $rootScope.$broadcast(CUSTOMER_EVENTS.selectParam);
+
+        if (!index) {
+          $scope.minSecondDate = moment($scope.firstDate).add(1, 'days').toDate();
+        }
       };
     }
   });
